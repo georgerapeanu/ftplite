ba557898d8f7f339d366fe75b364f4df91bc867b
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-11-29 20:41:58 +0200

    initial setup

---
+++ src/client/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test.h"
+
+using namespace std;
+
+int main() {
+  cout << get_output() << "\n";
+  return 0;
+}
---
+++ src/client/test.cpp
@@ -0,0 +1,5 @@
+#include "test.h"
+
+std::string get_output() {
+  return "Hello from client";
+}
