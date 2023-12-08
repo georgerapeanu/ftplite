128e1ab186ec19f51d4b91c6bf351d86e6e75215
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-12-08 11:58:46 +0200

    attempts to fix action

--- test-e2e/client/start_client.sh
+++ test-e2e/client/start_client.sh
@@ -1,14 +1,11 @@
 set -e && cd test-e2e/client && sudo chown ftplite-server client* && sudo chgrp ftplite-server client* && sleep 10 && ./ftp_commands.sh | valgrind ../../target/bin/client localhost 27015 && sleep 5 && echo "Showing diff" 1>&2 &&
+set -e && 
 for i in `ls`; do 
+  sudo diff /bin/sh /bin/ls 
   if [ -x $i ] ; then
     :
   else 
-    diff_msg=$(sudo diff $i ../server/$i);
-    if [ -n "${diff_msg}%?" ] ; then 
-      echo 1>&2 "$diff_msg"
-      echo 1>&2 "Diff failed for $i"
-      exit 1 
-    fi
+    sudo diff $i ../server/$i
   fi
 done
 
